8e050a1a3d5998722d90a2444403610670bec7bc
Andrzej Kebab <73285275+AndrzejKebab@users.noreply.github.com>
2021-10-15 12:57:46 +0200

    sasdada
    sdaad

--- Arkusz EE.09-03-19.06/zgloszenie.php
+++ Arkusz EE.09-03-19.06/zgloszenie.php
@@ -12,9 +12,9 @@
     $data = $_POST['data'];
     $sedzia = $_POST['sedzia'];
 
-    $db = new mysqli('localhost', 'root', '', 'wedkarstwo')
+    $db = new mysqli('localhost', 'root', '', 'wedkarstwo');
     $sql = "INSERT INTO `zawody_wedkarskie`(`Karty_wedkarskie_id`, `Lowisko_id`, `data_zawodow`, `sedzia`) VALUES (NULL, `2`, `$lowisko`, `$data` ,`$sedzia`)";
-    $db->querry($sql);
+    $db->query($sql);
     $db->close();
     ?>
 </body>
